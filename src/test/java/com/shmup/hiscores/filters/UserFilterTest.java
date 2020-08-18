6abef9294e5409c0b9032e331cfb3968d4b5f95d
jsmadja <xedy4bsa>
2020-08-18 18:46:50 +0200

    Test refactoring

--- src/test/java/com/shmup/hiscores/filters/UserFilterTest.java
+++ src/test/java/com/shmup/hiscores/filters/UserFilterTest.java
@@ -4,32 +4,37 @@ import com.shmup.hiscores.models.Player;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
 import java.io.IOException;
 
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class UserFilterTest {
 
     private UserFilter userFilter = new UserFilter();
 
+    @Mock(extraInterfaces = HttpServletRequest.class)
+    private ServletRequest request;
+
+    @Mock
+    private FilterChain filterChain;
+
+    @Mock
+    private ServletResponse response;
+
     @Test
     public void should_return_guest_if_no_shmup_cookie() throws IOException, ServletException {
-        ServletRequest request = Mockito.mock(HttpServletRequest.class);
-        ServletResponse response = Mockito.mock(ServletResponse.class);
-        FilterChain filterChain = Mockito.mock(FilterChain.class);
         userFilter.doFilter(request, response, filterChain);
 
         verify(request).setAttribute("player", Player.guest);
+        verify(filterChain).doFilter(request, response);
     }
 
 }
