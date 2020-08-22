f4bd35ddce615e354edd59f40198a6c39f1a926e
jsmadja <xedy4bsa>
2020-08-22 21:38:52 +0200

    Fix CORS

--- src/main/java/com/shmup/hiscores/HiscoresApplication.java
+++ src/main/java/com/shmup/hiscores/HiscoresApplication.java
@@ -3,6 +3,9 @@ package com.shmup.hiscores;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableCaching
@@ -12,4 +15,13 @@ public class HiscoresApplication {
 		SpringApplication.run(HiscoresApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
