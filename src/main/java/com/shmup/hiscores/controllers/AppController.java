a2196c4cb7d4fdfe897c2d83de086e909d148138
jsmadja <xedy4bsa>
2020-09-06 09:24:32 +0200

    Testing AppController

--- src/main/java/com/shmup/hiscores/controllers/AppController.java
+++ src/main/java/com/shmup/hiscores/controllers/AppController.java
@@ -1,16 +1,13 @@
 package com.shmup.hiscores.controllers;
 
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Deprecated
-@AllArgsConstructor
 @RestController
 public class AppController {
 
-    @RequestMapping("/")
-    public String getHome() {
+    @GetMapping("/")
+    public String getIndex() {
         return "OK";
     }
 
